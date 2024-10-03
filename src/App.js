7a6bf1858e21b506f2125f86c17221379ba823b1
Ken <kendev209@gmail.com>
2024-10-03 20:52:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+
+export default function App(){
+
+  const [step, setStep] = useState(2);
+
+  function handlePrevious(){
+    setStep(step-1>0?step-1:3);
+  }
+  function handleNext(){
+    setStep(step+1>3 ? 1 : step+1);
+  }
+  return (<div className="steps">
+   <div className="numbers">
+    <div className={step>=1 ? "active":""}>1</div>
+    <div className={step>=2 ? "active":""}>2</div>
+    <div className={step>=3 ? "active":""}>3</div>
+   </div>
+   <div className="message">Step {step} : {messages[step-1]}</div>
+   <div className="buttons">
+    <button style={{backgroundColor:'#7950f2', color:'#fff'}} 
+    onClick={handlePrevious}>Previous</button>
+    <button style={{backgroundColor:'#7950f2', color:'#fff'}}
+    onClick={handleNext}>Next</button>
+
+   </div>
+  </div>)
+}
